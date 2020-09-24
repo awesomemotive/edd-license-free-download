bb6dffd988baac4c48dff3d55cd359ca32fe3e55
Robin Cornett <hello@robincornett.com>
2020-09-24 09:18:48 -0400

    Prevent disabled keys from validating
    #6

--- edd-license-free-download.php
+++ edd-license-free-download.php
@@ -263,7 +263,7 @@ if( ! class_exists( 'EDD_License' ) ) {
 		if ( ! $license ) {
 			return false;
 		}
-		if ( ! in_array( $license->status, array( 'expired', 'revoked' ), true ) ) {
+		if ( ! in_array( $license->status, array( 'expired', 'revoked', 'disabled' ), true ) ) {
 			return true;
 		}
 
